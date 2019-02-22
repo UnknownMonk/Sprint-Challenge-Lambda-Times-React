007d3660341147ad27319f59f3ca8d17f3d05fb9
UnknownMonk <smodcartoon@gmail.com>
2019-02-22 11:39:49 -0500

    added card content and prop-types libary

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -5,7 +5,10 @@ import PropTypes from 'prop-types';
 const Cards = ({ cards }) => {
   return (
     <div className="cards-container">
-      {cards.map((c, index) => (
+      {/* Using the cards prop, map over the list creating a 
+          new Card component for each passing the card as the only prop*/
+
+      cards.map((c, index) => (
         <Card key={index} headline={c.headline} img={c.img} author={c.author} />
       ))}
     </div>
@@ -13,5 +16,15 @@ const Cards = ({ cards }) => {
 };
 
 // Make sure you include prop types for all of your incoming props
+
+// Cards.propType = {
+//   cards: PropTypes.arrayof(
+//     PropTypes.shape({
+//       headline: PropTypes.string,
+//       img: PropTypes.string,
+//       author: PropTypes.string
+//     })
+//   )
+// };
 
 export default Cards;
